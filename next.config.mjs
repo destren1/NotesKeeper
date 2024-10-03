0489f097b5ddf99ad5bbbce24cc3a36f9a35b006
Romanovskiy Ivan <ivanromanovskiy1103@gmail.com>
2024-10-03 21:56:23 +0300

    feat: deploy setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/NotesKeeper',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
